35d310a549a5ad7aec1cfb8277811351bf7fc276
Rodney Gainous Jr <rodney.gainous@gmail.com>
2024-07-02 13:57:44 -0700

    Initial Commit

---
+++ JustTalk/JustTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustTalkApp.swift
+//  JustTalk
+//
+//  Created by Rodney Gainous Jr on 7/2/24.
+//
+
+import SwiftUI
+
+@main
+struct JustTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
